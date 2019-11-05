96a5bca509e4d1f2230236b00645c4c34c3911f1
Angel R <angel.resendiz@sportsworld.com.mx>
2019-11-05 12:32:59 -0600

    Updates

---
+++ precache-manifest.5ab7a78366cad592a0364da04289fd3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9e97a0e61d06080fdc4493c65e07ac",
+    "url": "/encuestas/index.html"
+  },
+  {
+    "revision": "ed44a5f354cc37bb1b70",
+    "url": "/encuestas/static/css/main.6b981a00.chunk.css"
+  },
+  {
+    "revision": "2f27c9df0e5a75f25a39",
+    "url": "/encuestas/static/js/2.a7d59d75.chunk.js"
+  },
+  {
+    "revision": "ed44a5f354cc37bb1b70",
+    "url": "/encuestas/static/js/main.7b14f0f8.chunk.js"
+  },
+  {
+    "revision": "194ababa5fca7fab4aeb",
+    "url": "/encuestas/static/js/runtime-main.955c129f.js"
+  }
+]);
